d8196a6d09aadb060be6a25b1b99788daaa8ef0b
BokkaRakesh <bokkarakesh.b@gmail.com>
2024-11-29 17:44:35 +0530

    UI changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'zenserProject' title`, () => {
+  it(`should have the 'praticeProject' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('zenserProject');
+    expect(app.title).toEqual('praticeProject');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, zenserProject');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, praticeProject');
   });
 });
